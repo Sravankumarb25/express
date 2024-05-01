26fbfa4fc60cd33c7998ed179b107097b5952a5a
Sravan Kumar <bhaskarasravan.kumar@gaiansolutions.com>
2024-05-01 15:24:23 +0530

    waste

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
-const fs = require('fs');
-const path = require('path');
-const { table } = require('console');
+
+
+
 const cors = require('cors');
 const app = express();
 const port = 3002;
